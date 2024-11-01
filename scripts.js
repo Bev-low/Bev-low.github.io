84e9d06441ef9b26e51e0044a310e3a470cbeb28
BevLow <lowbeverlypersonal@gmail.com>
2024-11-01 22:19:34 +0800

    Update the buttons

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-function openTab(tabName) {
-    // Hide all elements with class="tab-content" by default
-    var i, tabcontent;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
 
-    // Show the current tab
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(sectionId).style.display = 'block';
 }
+
