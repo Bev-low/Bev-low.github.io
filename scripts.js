3c408c0ffcdb674a7c105bbe55388201e1bf90fe
BevLow <lowbeverlypersonal@gmail.com>
2024-11-01 22:40:32 +0800

    Update the buttons

--- scripts.js
+++ scripts.js
@@ -1,16 +1,8 @@
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
+function showPopup(hobbyId) {
+    document.getElementById(hobbyId).style.display = "flex";
+}
 
-    document.getElementById(sectionId).style.display = 'block';
-
-    const tabs = document.querySelectorAll('.nav-tabs a');
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
-
-    document.querySelector(`.nav-tabs a[href="#${sectionId}"]`).classList.add('active');
+function closePopup(hobbyId) {
+    document.getElementById(hobbyId).style.display = "none";
 }
 
